30838cdfe5c3341b4a2556ce3bf0037d68f36178
Adrian Moreno <amorenoz@redhat.com>
2022-11-02 11:53:35 +0100

    main: honor subcommands
    There's only one subcommand for now but, start making main aware of them
    and act accordingly.
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,20 @@ mod collector;
 mod config;
 mod core;
 use collector::get_collectors;
-use config::Cli;
+use config::{Cli, SubCommand};
 
 fn main() -> Result<()> {
     let mut cli = Cli::new()?;
     let mut collectors = get_collectors()?;
     collectors.register_cli(&mut cli)?;
     cli.parse()?;
-    collectors.init(&cli)?;
-    collectors.start(&cli)?;
+    let subcommand = cli.get_subcommand();
+    match subcommand.expect("no subcommand") {
+        SubCommand::Collect(collect) => {
+            println!("Collecting {:#?}", collect);
+            collectors.init(&cli)?;
+            collectors.start(&cli)?;
+        }
+    }
     Ok(())
 }
