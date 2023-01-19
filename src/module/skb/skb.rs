d10944840e5241d4556040d1120fa4aa89616687
Antoine Tenart <atenart@redhat.com>
2023-01-19 17:37:17 +0100

    module: skb: dev event section support
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- src/module/skb/skb.rs
+++ src/module/skb/skb.rs
@@ -21,7 +21,7 @@ pub(crate) struct SkbCollectorArgs {
         long,
         help = "Comma separated list of data to collect from SKBs.
 
-Possible values: all, l2, l3, tcp, udp, icmp.
+Possible values: all, l2, l3, tcp, udp, icmp, dev.
 Default value: l3,tcp,udp,icmp."
     )]
     skb_sections: Option<String>,
@@ -65,6 +65,7 @@ impl Collector for SkbCollector {
                     "tcp" => sections |= 1 << SECTION_TCP,
                     "udp" => sections |= 1 << SECTION_UDP,
                     "icmp" => sections |= 1 << SECTION_ICMP,
+                    "dev" => sections |= 1 << SECTION_DEV,
                     x => bail!("Unknown skb_collect value ({})", x),
                 }
             }
@@ -88,6 +89,7 @@ impl Collector for SkbCollector {
                     SECTION_TCP => unmarshal_tcp(raw_section, fields),
                     SECTION_UDP => unmarshal_udp(raw_section, fields),
                     SECTION_ICMP => unmarshal_icmp(raw_section, fields),
+                    SECTION_DEV => unmarshal_dev(raw_section, fields),
                     _ => bail!("Unknown data type"),
                 },
             ),
